aa5660b3cd18ad61186f3dd2542a9e154adc4d21
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-03-19 02:48:19 -0400

    Add a binary to the example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
+#![allow(clippy::needless_return)]
 
 use core::panic::PanicInfo;
 use libc_print::std_name::{print, println};
 use cursednoise::{NoiseBuilder, Settings, SimplexSettings};
 
-fn main() {
+#[unsafe(no_mangle)]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize{
 
     let AAAA = NoiseBuilder::fbm_3d(3, 3, 3).with_freq(0.01).with_seed(4002).with_octaves(3).generate_scaled(0.0, 5.0);
 
@@ -15,6 +17,8 @@ fn main() {
         }
         println!()
     }
+
+    return 0;
 }
 
 #[panic_handler]
